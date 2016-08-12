8e93db2b9a162e324da722f52b44f7553b134b0d
darshanrane <darshanyrane@gmail.com>
2016-08-12 21:10:13 +0530

    Adding Bulls and Cows to Repo

---
+++ bullsandcows/README.md
@@ -0,0 +1,12 @@
+bullsandcows
+============
+
+Its simple number game where
+the player has to guess a 4 digit number.
+All the digits in the number are unique/different.
+
+If the matching digits are on the right position
+they are counted as "Bulls".
+
+If they are on different positions
+they are counted as "Cows".
---
+++ bullsandcows/js/bullsandcows.js
@@ -0,0 +1,135 @@
+var nums;
+var tries = 0;
+var num = new Array();
+
+$(document).ready(function() 
+{	
+	$("#playBtn").click(function(){
+		
+		$("#screen1").hide();
+		$("#screen2").show();
+		
+		$("#playBtn").hide();
+		$("#guessBtn").show();
+		$("#resetBtn").css("opacity", 1);
+		$("#clearBtn").css("opacity", 1);
+		$("#numContainer").css("opacity", 1);
+		
+		initGame();		
+		
+	});
+	
+
+});
+
+function initGame()
+{
+	getRandom();
+	
+	$(".num").each(function() {
+		
+		$(this).click(function(){	
+		
+			if($("#userInput").text() == "0000")
+				$("#userInput").empty();
+				
+			if($("#userInput").text().length < 4)
+			{
+				$("#userInput").append(this.id.substr(3));
+				num.push(this.id.substr(3));
+			}
+		});
+		
+	});
+	
+	$("#clearBtn").click(function(){
+		
+		if($("#userInput").text() == "0000")
+			$("#userInput").empty();
+	
+		if($("#userInput").text().length > 0)
+		{
+			$("#userInput").html($("#userInput").text().substr(0, $("#userInput").text().length - 1));
+			num.pop();
+		}
+	});
+	
+	$("#resetBtn").click(function(){
+		getRandom();
+		$("#userInput").html('0000');
+		$("#userPreContainer").html('');
+		tries = 0;
+		$("#cows").html('0');
+		$("#bulls").html('0');
+	});
+	
+	$("#guessBtn").click(function(){
+		checkNum();
+	});
+}
+
+function getRandom()
+{
+	nums = new Array();
+	for(i = 0; i < 4; i++)
+	{
+		var add = true;
+		var n = Math.floor(Math.random()*9) + 1;
+		for(j = 0; j < 4; j++)
+		{
+			if(nums[j] == n)
+				add = false;
+		}
+		if(add)
+			nums.push(n)
+		else
+			i--;
+	}
+}
+
+function checkNum()
+{
+	var c=0;
+	var b=0;
+	
+	for(var i = 0; i < 4; i++)
+	{
+		if(nums[i] == num[i])
+			b++;
+	}
+	
+	for(var i = 0; i < 4; i++)
+	{
+		for(var j = 0; j < 4; j++)
+		{
+			if(nums[j] == num[i] && j != i)
+				c++;
+		}
+	}
+	
+	$("#cows").html(c);
+	$("#bulls").html(b);
+	
+	var userstr = "";
+	for (k in num)
+    	userstr += (num[k]);
+	
+	$("#userPreContainer").append("<div class='userPreInput'>"+ userstr +"</div>");
+	$("#userInput").html('');
+	num = new Array();
+	
+	if(b == 4)
+	{
+		alert("Thats Right!!!! Its " + nums);
+		$("#userInput").html('0000');
+		$("#cows").html('0');
+		$("#bulls").html('0');
+		tries = 0;
+                num = new Array();
+		$("#userPreContainer").empty();		
+		getRandom();
+	}
+	else
+	  tries++;
+
+}
